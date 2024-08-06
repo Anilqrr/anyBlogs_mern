9c8479cabbb798fd9faaa7d61e5bfa02ed320d38
Anil Kantariya <101086982+Anilqrr@users.noreply.github.com>
2024-08-06 22:15:46 +0530

    Update UserView.js path

--- anyBlogs/src/redux/UserView.js
+++ anyBlogs/src/redux/UserView.js
@@ -9,7 +9,7 @@ export const UserProfileView = createAsyncThunk(
   'user/profile/view',
   async (id) => {
 
-    const response = await axios.post(`https://any-blogs.vercel.app/api/user/profile/view/${id}`)
+    const response = await axios.post(`https://any-blogs-server.vercel.app/api/user/profile/view/${id}`)
       .then((response) => {
 
         return response.data
